92a9a95050170460d643f4a194b0be01996a55bb
La823 <lavanyaarora823@gmail.com>
2025-04-19 18:27:35 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"; 
+
+export default function Home() {
+  return (
+  <>Hello admin dashboard
+  <div className="p-4">
+  <Button size="icon" variant="link" >here</Button>
+  </div>
+  </>
+  );
+
+}
